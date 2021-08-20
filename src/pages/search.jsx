03eb012dbe05b08ba0b6baf02d033600b2501d5a
Jose Sitanggang <josealfredositanggang@gmail.com>
2021-08-21 06:28:04 +0700

    update:

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Flex, Input, Stack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
-import { GiftList } from "../components";
+import { GiftList, SearchInput } from "../components";
 import { useGiphySearchAPI } from "../hooks";
 
 export function SearchPage(props) {
@@ -15,20 +15,7 @@ export function SearchPage(props) {
       align="center"
       direction="column"
     >
-      <Stack
-        w="100%"
-        padding={[2, 6, 8]}
-        mt="4"
-        mb="12"
-        minWidth="max-content"
-        spacing="6"
-      >
-        <Input
-          variant="filled"
-          placeholder="Search here..."
-          onChange={(e) => updateKeyword(e.target.value)}
-        />
-      </Stack>
+      <SearchInput onChange={(e) => updateKeyword(e.target.value)} />
       <GiftList gifs={gifs} />
     </Flex>
   );
